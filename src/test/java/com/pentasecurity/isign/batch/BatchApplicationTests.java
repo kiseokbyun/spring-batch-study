8fab892fc941d8fe587dd43324532bb8c390fcaa
kiseokbyun <qusrl0715@gmail.com>
2020-03-27 18:23:49 +0900

    init project

---
+++ src/test/java/com/pentasecurity/isign/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentasecurity.isign.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
