86ad601462d4d38594024ef407a39308a40b5d2d
kiseokbyun <qusrl0715@gmail.com>
2020-05-01 14:05:00 +0900

    JdbcCursorItemReaderJobConfiguration.java

---
+++ src/main/java/com/ksbyun/batch/study/job/JdbcCursorItemReaderJobConfiguration.java
@@ -0,0 +1,66 @@
+package com.ksbyun.batch.study.job;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.ksbyun.batch.study.job.model.Pay;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcCursorItemReaderJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    private static final int FETCH_SIZE = 10;
+    private static final int CHUNK_SIZE = 10;
+    
+    @Bean
+    public Job jdbcCursorItemReaderJob() {
+        return jobBuilderFactory.get("jdbcCursorItemReaderJob")
+                .start(jdbcCursorItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcCursorItemReaderStep() {
+        return stepBuilderFactory.get("jdbcCursorItemReaderStep")
+                .<Pay, Pay>chunk(CHUNK_SIZE)
+                .reader(jdbcCursorItemReader())
+                .writer(jdbcCursorItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Pay> jdbcCursorItemReader() {
+        return new JdbcCursorItemReaderBuilder<Pay>()
+                .fetchSize(FETCH_SIZE)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+                .name("jdbcCursorItemReader")
+                .build();
+    }
+
+    public ItemWriter<? super Pay> jdbcCursorItemWriter() {
+        return items -> {
+            for (Pay item : items) {
+                log.info("Current Pay = {}", item);
+            }
+        };
+    }
+}
