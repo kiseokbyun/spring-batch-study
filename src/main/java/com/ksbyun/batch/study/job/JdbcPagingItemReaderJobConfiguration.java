bfce1c4f6a95f66c18dd49bfdb80e2f8d887c735
kiseokbyun <qusrl0715@gmail.com>
2020-05-02 15:15:00 +0900

    JdbcPagingItemReaderJobConfiguration.java

---
+++ src/main/java/com/ksbyun/batch/study/job/JdbcPagingItemReaderJobConfiguration.java
@@ -0,0 +1,94 @@
+package com.ksbyun.batch.study.job;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.ksbyun.batch.study.job.model.Pay;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcPagingItemReaderJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    
+    private static final int CHUNK_SIZE = 10;
+    private static final int PAGE_SIZE = 10;
+    private static final int FETCH_SIZE = 10;
+    
+    @Bean
+    public Job jdbcPagingItemReaderJob() throws Exception {
+        return jobBuilderFactory.get("jdbcPagingItemReaderJob")
+                .start(jdbcPagingItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcPagingItemReaderStep() throws Exception {
+        return stepBuilderFactory.get("jdbcPagingItemReaderStep")
+                .<Pay, Pay>chunk(CHUNK_SIZE)
+                .reader(jdbcPagingItemReader())
+                .writer(jdbcPagingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Pay> jdbcPagingItemReader() throws Exception {
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("amount", 2000);
+        
+        return new JdbcPagingItemReaderBuilder<Pay>()
+                .pageSize(PAGE_SIZE)
+                .fetchSize(FETCH_SIZE)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .queryProvider(createQueryProvider())
+                .parameterValues(parameterValues)
+                .name("jdbcPagingItemReader")
+                .build();
+    }
+    
+    public ItemWriter<Pay> jdbcPagingItemWriter() {
+        return items -> {
+            for (Pay item : items) {
+                log.info("Current Pay = {}", item);
+            }
+        };
+    }
+
+    @Bean
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProviderFactoryBean = new SqlPagingQueryProviderFactoryBean();
+        queryProviderFactoryBean.setDataSource(dataSource);
+        queryProviderFactoryBean.setSelectClause("id, amount, tx_name, tx_date_time");
+        queryProviderFactoryBean.setFromClause("from pay");
+        queryProviderFactoryBean.setWhereClause("where amount >= :amount");
+        
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+        
+        queryProviderFactoryBean.setSortKeys(sortKeys);
+        
+        return queryProviderFactoryBean.getObject();
+    }
+}
